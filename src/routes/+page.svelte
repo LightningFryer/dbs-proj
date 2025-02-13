82e1c2923e954616d89be407804af98566a8fb30
LightningFryer <91123163+LightningFryer@users.noreply.github.com>
2025-02-13 23:26:42 +0530

    more updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { turso } from '$lib/db';
-	import { onMount } from 'svelte';
 	import type { PageProps } from './$types';
 	let { data }: PageProps = $props();
+
+	console.log(data.rows);
 </script>
 
 <div class="">
@@ -10,31 +10,21 @@
 		<thead>
 			<tr>
 				<th>Hotel</th>
-                <th>Year Booked</th>
-                <th>Month Booked</th>
-                <th>Weekend Nights Booked</th>
-                <th>Weekeday Nights Booked</th>
-                <th>No. of Adults</th>
-                <th>No. of Children</th>
-                <th>No. of Babies</th>
+				<th>No. of Adults</th>
 			</tr>
 		</thead>
 		<tbody>
 			{#each data.rows as row}
 				<tr>
-					<th><input type="text" value={row[0] as string} class="input"/></th>
-					<th><input type="text" value={row[1] as string} class="input"/></th>
-					<th><input type="text" value={row[2] as string} class="input"/></th>
-					<th><input type="text" value={row[3] as string} class="input"/></th>
-					<th><input type="text" value={row[4] as string} class="input"/></th>
-					<th><input type="text" value={row[5] as string} class="input"/></th>
-					<th><input type="text" value={row[6] as string} class="input"/></th>
-					<th><input type="text" value={row[7] as string} class="input"/></th>
+					<!-- <th><input type="text" value={row[0] as string} class="input"/></th> -->
+					<!-- <th><input type="text" value={row[1] as string} class="input"/></th> -->
+					<th><p>{row[0] as string}</p></th>
+					<th><p>{row[1] as string}</p></th>
 					<th class="flex flex-row">
 						<button class="btn">Update</button>
 						<button class="btn">Delete</button>
 					</th>
-                </tr>
+				</tr>
 			{/each}
 		</tbody>
 	</table>
